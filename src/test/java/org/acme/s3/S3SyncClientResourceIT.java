6ccc18090d9988f7d712b5cb526b1b621c124cd7
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2023-04-21 20:21:22 +0200

    first commit

---
+++ src/test/java/org/acme/s3/S3SyncClientResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme.s3;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class S3SyncClientResourceIT extends S3SyncClientResourceTest {
+    // Execute the same tests but in packaged mode.
+}
